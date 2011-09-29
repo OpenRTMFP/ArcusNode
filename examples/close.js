bad9331e60388bca0a6547572ce9eb2c1b4fe87c
Sebastian Herrlinger <sebastian@formzoo.com>
2011-09-29 15:33:55 -0700

    Add callbacks to api methods + fix address mixup

--- examples/close.js
+++ examples/close.js
@@ -7,9 +7,19 @@ var ArcusNode = require('../lib/arcus_node.js');
 var arc = ArcusNode.createServer();
 
 arc.on('connect', function(nc, options, username, password){
+	nc.on('close', function(){
+    console.log('Connection ' + nc.id + ' was closed.');
+  });
+
   // Close the connection after 30 seconds
   setTimeout(function() {
-  	nc.close();
+  	nc.close(function(success){
+     if(success){
+       console.log('Connection close attempt successful.');
+     } else {
+       console.log('ERROR: Connection could not be closed.');
+     }
+    });
   }, 3000);
 });
 
