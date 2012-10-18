f72fbd2bbe475a41d74302b78a35bf58800df495
Sebastian Herrlinger <sebastian@formzoo.com>
2012-10-18 14:45:46 +0200

    Add unix build script

---
+++ dev/FLDev/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash  
+
+# Exit on error
+set -e
+
+BUILD_PATH="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Checking path to repository
+echo "Build path directory: $BUILD_PATH"
+
+echo "Executing as user $(whoami)"
+
+# Create build dir
+if [ ! -d "$BUILD_PATH/bin" ]; then
+  mkdir "$BUILD_PATH/bin" && chown -R "$(whoami).$(whoami)" "$BUILD_PATH/bin"
+fi
+
+# Remove previous build
+rm -f "$BUILD_PATH/bin/fldev.swf"
+
+mxmlc -output="$BUILD_PATH/bin/fldev.swf" -source-path+="$BUILD_PATH/lib" --target-player=11.2 -debug -swf-version 16 "$BUILD_PATH/src/Main.as" -static-link-runtime-shared-libraries=true
